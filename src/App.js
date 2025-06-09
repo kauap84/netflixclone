e6151f8293b327846c667ea2871b86952bfc222d
kauap84 <kauavierazambolin@gmail.com>
2025-06-09 17:41:41 -0300

    aula09-06

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React, {useState, useEffect} from 'react';
-import tmdb from './tmdb';
-import MovieRow from './components/MovieRow.js';
 
-export default () => {
+import React, { useEffect, useState } from 'react';
+import Tmdb from './tmdb';
+import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
+import Header from './components/Header';
+import './App.css';
+
+export default function App() {
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
 
-    useEffect(() => {
-      const loadAll =async () => {
-        let list = await tmdb.getHomeList();
-        setMovieList(list)
+  useEffect(() => {
+   
+    const loadAll = async () => {
+     
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+    
+      let originals = list.filter(i => i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * originals[0].items.results.length);
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
+    };
+
+    loadAll();
+  }, []);
+
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
       }
-      loadAll();
-    }, [])
+    };
 
-    return (
-      <div className='page'>
-        <section className='lists'>
-            {movieList.map((item, key) => (
-              <MovieRow key={key} title={item.title} items={item.items}/>
-            ))}
-        </section>
+    window.addEventListener('scroll', scrollListener);
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+    };
+  }, []);
 
-      </div>
-    )
+  return (
+    <div className="page">
+      <Header black={blackHeader} />
+
+      {featuredData && <FeaturedMovie item={featuredData} />}
+
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+
+      <footer>
+        Feito Por Victor de Padua <br />
+        Direitos de imagem para Netflix<br />
+        Dados fornecidos por <a href="https://www.themoviedb.org/" target="_blank" rel="noreferrer">TMDB</a>
+      </footer>
+
+      {movieList.length <= 0 && 
+        <div className="loading">
+          <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="Carregando" />
+        </div>
+      }
+    </div>
+  );
 }
--- src/tmdb.js
+++ src/tmdb.js
@@ -16,9 +16,9 @@ const basicFetch = async (endpoint) => {
     const req = await fetch(`${API_BASE}${endpoint}`);
     const json = await req.json();
     return json;
-}
+};
 
-export default {
+const Tmdb = {
     getHomeList: async () => {
         return [
             {
@@ -62,5 +62,27 @@ export default {
                 items: await basicFetch(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}&sort_by=popularity.desc`),
             }
         ];
+    },
+
+    getMovieInfo: async (movieId, type) => {
+        let info = {};
+
+        if (movieId) {
+            switch (type) {
+                case 'movie':
+                    info = await basicFetch(`/movie/${movieId}?language=pt-BR&api_key=${API_KEY}`);
+                    break;
+                case 'tv':
+                    info = await basicFetch(`/tv/${movieId}?language=pt-BR&api_key=${API_KEY}`);
+                    break;
+                default:
+                    info = null;
+                    break;
+            }
+        }
+
+        return info;
     }
-}
+};
+
+export default Tmdb;
